0a78757895c29480780710162889dea6854e6282
urso <steffen.siering@elastic.co>
2016-05-25 12:34:38 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,17 @@
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-export DURATION=240
+export DURATION=45
+export WAIT_COLLECT=10
 export ENABLE_WATCH=false
 
 while true; do
-    #./genbeat_logstash/compressed/run.sh
+    ./genbeat_logstash/compressed/run.sh
+    ./genbeat_logstash/compressed_pipelined/run.sh
     #./genbeat_logstash/uncompressed_pipelined/run.sh
+
     ./genbeat_tstlj_v2/compressed/run.sh
     ./genbeat_tstlj_v2/compressed_pipelined/run.sh
-    #./genbeat_tstlj_es/run.sh
+
+    ./genbeat_tstlj_es/run.sh
 done
